661446fa00b99d7075bed74c40baac16341ee4f2
zdm <zdm@softvisio.net>
2021-05-24 12:24:40 +0300

    tests added

---
+++ __tests__/000-result.js
@@ -0,0 +1,73 @@
+// import { jest } from "@jest/globals";
+
+import result from "../lib/_browser/index.js";
+
+test( "1", () => {
+    const res = result( 200 );
+
+    expect( res.toJSON() ).toStrictEqual( {
+        "status": 200,
+        "reason": "OK",
+        "exception": false,
+        "data": undefined,
+    } );
+} );
+
+test( "2", () => {
+    const res = result( 200, "data", { "a": 1, "b": 2 } );
+
+    expect( res.toJSON() ).toStrictEqual( {
+        "status": 200,
+        "reason": "OK",
+        "exception": false,
+        "data": "data",
+        "a": 1,
+        "b": 2,
+    } );
+} );
+
+test( "3", () => {
+    const res = result( [200] );
+
+    expect( res.toJSON() ).toStrictEqual( {
+        "status": 200,
+        "reason": "OK",
+        "exception": false,
+        "data": undefined,
+    } );
+} );
+
+test( "4", () => {
+    const res = result( [200, "reason"] );
+
+    expect( res.toJSON() ).toStrictEqual( {
+        "status": 200,
+        "reason": "reason",
+        "exception": false,
+        "data": undefined,
+    } );
+} );
+
+test( "5", () => {
+    const res = result( [200, false] );
+
+    expect( res.toJSON() ).toStrictEqual( {
+        "status": 200,
+        "reason": "OK",
+        "exception": false,
+        "data": undefined,
+    } );
+} );
+
+test( "6", () => {
+    const _res = result( [300, "reason"], "data", { "a": 1, "b": 2 } );
+
+    const res = result( _res );
+
+    expect( res.toJSON() ).toStrictEqual( {
+        "status": 300,
+        "reason": "reason",
+        "exception": false,
+        "data": undefined,
+    } );
+} );
