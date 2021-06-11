b1344ba1304165cebc858bc3a13f345875397ff2
zdm <zdm@softvisio.net>
2021-06-11 14:06:45 +0300

    support for Result-like objects added

---
+++ resources/status-json-rpc.js
@@ -0,0 +1,8 @@
+export default {
+    "-32700": "Parse error",
+    "-32600": "Invalid Request",
+    "-32601": "Method not found    ",
+    "-32602": "Invalid params",
+    "-32603": "Internal error",
+    "-32000": "Server error", // -32000 to -32099
+};
