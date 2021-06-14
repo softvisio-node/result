7c621162c3a9067fc89b5b6b22924da6b14098e4
zdm <zdm@softvisio.net>
2021-06-14 21:18:23 +0300

    rpc response codes updated

--- resources/status-json-rpc.js
+++ resources/status-json-rpc.js
@@ -4,6 +4,8 @@
 // XML-RPC at the following url: http://xmlrpc-epi.sourceforge.net/specs/rfc.fault_codes.php
 
 export default {
+
+    // don't use standard error codes in api response, create custom code, if needed
     "-32700": [400, "Parse Error"],
     "-32600": [400, "Invalid Request"],
     "-32601": [404, "Method Not Found"],
@@ -11,12 +13,23 @@ export default {
     "-32603": [500, "Internal Error"],
     "-32000": [500, "Server Error"], // -32000 to -32099
 
-    // custom error codes
-    "-32800": [400, "RPC Calls Are Not Supported"],
-    "-32801": [400, "Insufficient Permissions"],
-    "-32802": [400, "Too Many Requests"],
-    "-32803": [415, "Invalid Content Type"],
-    "-32804": [405, "HTTP Method Not Allowed"],
-    "-32805": [413, "Payload Too Large"],
-    "-32806": [411, "Length Required"],
+    // custom common errors
+    "-32800": [400, "RPC calls are not supported"],
+    "-32801": [401, "Insufficient permissions to call API method"],
+    "-32802": [429, "Too many requests"],
+    "-32803": [415, "Unsupported content type"],
+    "-32804": [405, "HTTP method not allowed"],
+    "-32805": [403, "Private API method called"],
+    "-32806": [400, "Parameters parsing error"],
+    "-32807": [400, "Unable to decode RPC message body"],
+    "-32808": [400, "Parameters validation error"],
+    "-32809": [404, "API method not found"],
+
+    // custom upload errors
+    "-32900": [400, "Upload using websockets is not supported"],
+    "-32901": [411, "Upload size is required"],
+    "-32902": [413, "Upload too large"],
+    "-32903": [400, "API method not support uplads"],
+    "-32904": [400, "API upload method called as RPC"],
+    "-32905": [400, "Upload file is required"],
 };
