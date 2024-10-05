0480b9596704d2e22de1b41fe2fffac4c893a9a5
zdm <zdm@softvisio.net>
2024-10-05 04:22:26 +0300

    style: linted

--- _tests/result.test.js
+++ _tests/result.test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { suite, test } from "node:test";
 import assert from "node:assert";
+import { suite, test } from "node:test";
 import "#lib/index";
 
 suite( "result", () => {
